50896601b9984acce7d8f0d3a8276d587bd17881
Ryato-03 <116544189+Ryato-03@users.noreply.github.com>
2022-11-15 13:05:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,23 @@
 using namespace std;
 
 int main() {
-    int a, b, x, y;
+    float a, b, x, y;
     cin >> a >> b >> x >> y;
+    float risultato;
 
-    if ((x < 0) && (y > 0))
+    if ((x < 0 && y > 0))
     {
-        cout << (a * x - b * y) << endl;
+        risultato = a*x - b*y;
     }
-    else if ((x >= 0) && (y <= 0))
+    else if ((x >= 0 && y <= 0))
     {
-        cout << (a*(x*x) - b*y) << endl;
+        risultato = a*(x*x) - b*y;
     }
     else
     {
-        cout << (a*x - b*(y*y)) << endl;
+        risultato = a*x - b*(y*y);
     }
 
+    cout << risultato;
     return 0;
 }
